dce3b3a50428b04f3b1fa397ee0eff030ab9cf11
José Albano Gaspar <up202008561@edu.fe.up.pt>
2021-12-10 17:54:49 +0000

    end of example 2

--- src/main/java/pt/up/fe/ldts/example1/Order.java
+++ src/main/java/pt/up/fe/ldts/example1/Order.java
@@ -15,28 +15,22 @@ public class Order {
     }
 
     public boolean isElegibleForFreeDelivery() {
+        return getTotal()>100;
+    }
+
+    public double getTotal(){
         double total = 0;
 
         for (OrderLine line : lines)
-            total += line.product.getPrice() * line.quantity;
-
-        if (total > 100) return true;
-        else return false;
+            total += line.getTotal();
+        return total;
     }
 
     public String printOrder() {
         StringBuffer printBuffer = new StringBuffer();
-
         for (OrderLine line : lines)
-            printBuffer.append(line.product.getName() + "(x" + line.quantity + "): " + (line.product.getPrice() * line.quantity) + "\n");
-
-        double total = 0;
-
-        for (OrderLine line : lines)
-            total += line.product.getPrice() * line.quantity;
-
-        printBuffer.append("Total: " + total);
-
+            printBuffer.append(line.toString() + "\n");
+        printBuffer.append("Total: " + getTotal());
         return printBuffer.toString();
     }
 }
--- src/main/java/pt/up/fe/ldts/example1/OrderLine.java
+++ src/main/java/pt/up/fe/ldts/example1/OrderLine.java
@@ -4,8 +4,27 @@ public class OrderLine {
     public Product product;
     public int quantity;
 
+    public Product getProduct(){
+        return product;
+    }
+    public int getQuantity(){
+        return quantity;
+    }
+    public void setProduct(Product p){
+        this.product = p;
+    }
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
     public OrderLine(Product product, int quantity) {
         this.product = product;
         this.quantity = quantity;
+    }
+    public double getTotal() {
+        return getProduct().getPrice() * getQuantity();
+    }
+    @Override
+    public String toString() {
+        return getProduct().getName() + "(x" + getQuantity() + "): " + getTotal();
     }
 }
